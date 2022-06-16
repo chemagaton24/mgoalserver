76953ddf208ae98558abdbca6b7b31e44ddbb344
chemagaton <75408311+chemagaton@users.noreply.github.com>
2022-06-16 20:47:30 +0800

    init

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const { ApolloServer } = require("apollo-server");
 const { typeDefs } = require("./schema");
 const { resolvers } = require("./resolvers");
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const PORT = process.env.port || 4000
 
-server.listen().then(({ url }) => {
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+server.listen({port: PORT}).then(({ url }) => {
     console.log(`Now running on ${url}`);
 });
